52e840fae23c0d52972d05dfe43e9be6c306880a
nayere6342 <146861836+nayere6342@users.noreply.github.com>
2026-04-06 19:44:15 -0400

    Update data.py

--- saves/data.py
+++ saves/data.py
@@ -1,9 +1,57 @@
-# pipe top
-target = pygame.Rect(z, 0, 120, 190)
-collision = hit_box.colliderect(target)
-pygame.draw.rect(screen, (255 * collision, 255, 0), target)
+# This is just a save folder for unused data
 
-# pipe bottem
-target = pygame.Rect(z, 500, 120, 190)
-collision = hit_box.colliderect(target)
-pygame.draw.rect(screen, (255 * collision, 255, 0), target)
+# # pipe top
+# target = pygame.Rect(z, 0, 120, 190)
+# collision = hit_box.colliderect(target)
+# pygame.draw.rect(screen, (255 * collision, 255, 0), target)
+#
+# # pipe bottem
+# target = pygame.Rect(z, 500, 120, 190)
+# collision = hit_box.colliderect(target)
+# pygame.draw.rect(screen, (255 * collision, 255, 0), target)
+
+
+# # pipe top
+#     class Pipe2(pygame.sprite.Sprite):
+#         def __init__(self, x, y):
+#             super().__init__()
+#             self.image = pygame.image.load('Objects/pipe.png').convert()
+#             self.rect = self.image.get_rect()
+#             self.rect.topleft = (z, d)
+#             self.velocity = random.randint(1, 1)
+#
+#         def update(self):
+#             self.rect.y += self.velocity
+#
+#     # mov loop
+#     for i in range(1):
+#         pipe = Pipe2(i + 300, -20)
+#         pipe_group2.add(pipe)
+#     pipe_group2.draw(screen)
+#     pipe_group2.update()
+
+
+# sillynum = random.randint(1, 5)
+#
+# # mov loop
+# for i in range(1):
+#     # if (sillynum == 2):
+#     #     pipe_group.add(pipe)
+#     pipe = Pipe(i + 400, -20)
+# pipe_group.update()
+# pipe_group.draw(screen)
+
+# displays everything on the window
+# pygame.display.flip()
+
+
+# sillynum = random.randint(1, 5)
+
+
+# pipe_group.add(pipe)
+
+
+# d = 230
+
+
+# self.velocity = random.randint(1, 2)
