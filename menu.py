746a052b81166aaa3b5e1f3854534c7ae47269d7
nayere6342 <146861836+nayere6342@users.noreply.github.com>
2026-04-06 22:01:28 -0400

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Press:
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+        mpos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mpos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
